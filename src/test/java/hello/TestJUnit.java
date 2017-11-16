73c7641c7eae981c38044fca935c5b85a04e09e5
joanna.wojcik <joanna.wojcik@hotmail.com>
2017-11-16 22:25:20 +0000

    MQTT publishing and receiving of a message

--- src/test/java/hello/TestJUnit.java
+++ src/test/java/hello/TestJUnit.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertThat;
 
 import java.net.URL;
 
+import org.eclipse.paho.client.mqttv3.*;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,26 +16,19 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import org.eclipse.paho.client.mqttv3.MqttClient;
-import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
-import org.eclipse.paho.client.mqttv3.MqttException;
-import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class TestJUnit {
-
-    @Autowired
-    private TestRestTemplate template;
+public class TestJUnit implements MqttCallback {
 
     @Test
     public void getHello() throws Exception {
         String topic = "MQTT Examples";
         String content = "Message from MqttPublishSample";
         int qos = 2;
-        String broker = "tcp://localhost:1883";
+        String broker = "tcp://192.168.99.100:1883";
         String clientId = "";
         MemoryPersistence persistence = new MemoryPersistence();
 
@@ -42,15 +36,20 @@ public class TestJUnit {
             MqttClient sampleClient = new MqttClient(broker, clientId, persistence);
             MqttConnectOptions connOpts = new MqttConnectOptions();
             connOpts.setCleanSession(true);
+            sampleClient.setCallback(this);
             System.out.println("Connecting to broker: " + broker);
             sampleClient.connect(connOpts);
             System.out.println("Connected");
             System.out.println("Publishing message: " + content);
+
             MqttMessage message = new MqttMessage(content.getBytes());
             message.setQos(qos);
+            sampleClient.subscribe(topic);
             sampleClient.publish(topic, message);
+
             System.out.println("Message published");
             sampleClient.disconnect();
+
             System.out.println("Disconnected");
             System.exit(0);
         } catch (MqttException me) {
@@ -61,5 +60,20 @@ public class TestJUnit {
             System.out.println("excep " + me);
             me.printStackTrace();
         }
+    }
+
+    @Override
+    public void connectionLost(Throwable cause) {
+
+    }
+
+    @Override
+    public void messageArrived(String topic, MqttMessage message) throws Exception {
+        System.out.println("Message \"" + message + "\" received");
+    }
+
+    @Override
+    public void deliveryComplete(IMqttDeliveryToken token) {
+
     }
 }
